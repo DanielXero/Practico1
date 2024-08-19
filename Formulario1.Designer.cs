d47a219275dbb7cb829ff74957c1f2d10fd0df66
Gustavo Sosa <117545957+DanielXero@users.noreply.github.com>
2024-08-19 14:40:45 -0300

    Agregar archivos de proyecto.

---
+++ Formulario1.Designer.cs
@@ -0,0 +1,149 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace Practico1
+{
+    partial class Formulario1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BGuardar = new System.Windows.Forms.Button();
+            this.BEliminar = new System.Windows.Forms.Button();
+            this.LApellido = new System.Windows.Forms.Label();
+            this.LNombre = new System.Windows.Forms.Label();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.BSalir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BGuardar
+            // 
+            this.BGuardar.Location = new System.Drawing.Point(23, 133);
+            this.BGuardar.Name = "BGuardar";
+            this.BGuardar.Size = new System.Drawing.Size(64, 20);
+            this.BGuardar.TabIndex = 0;
+            this.BGuardar.Text = "Guardar";
+            this.BGuardar.UseVisualStyleBackColor = true;
+            this.BGuardar.Click += new System.EventHandler(this.BGuardar_Click_1);
+            // 
+            // BEliminar
+            // 
+            this.BEliminar.Location = new System.Drawing.Point(105, 133);
+            this.BEliminar.Name = "BEliminar";
+            this.BEliminar.Size = new System.Drawing.Size(64, 20);
+            this.BEliminar.TabIndex = 1;
+            this.BEliminar.Text = "Eliminar";
+            this.BEliminar.UseVisualStyleBackColor = true;
+            this.BEliminar.Click += new System.EventHandler(this.BEliminar_Click_1);
+            // 
+            // LApellido
+            // 
+            this.LApellido.AutoSize = true;
+            this.LApellido.Location = new System.Drawing.Point(23, 62);
+            this.LApellido.Name = "LApellido";
+            this.LApellido.Size = new System.Drawing.Size(44, 13);
+            this.LApellido.TabIndex = 2;
+            this.LApellido.Text = "Apellido";
+            // 
+            // LNombre
+            // 
+            this.LNombre.AutoSize = true;
+            this.LNombre.Location = new System.Drawing.Point(23, 92);
+            this.LNombre.Name = "LNombre";
+            this.LNombre.Size = new System.Drawing.Size(44, 13);
+            this.LNombre.TabIndex = 3;
+            this.LNombre.Text = "Nombre";
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(83, 59);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(86, 20);
+            this.txtApellido.TabIndex = 4;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(83, 89);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(86, 20);
+            this.txtNombre.TabIndex = 5;
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Location = new System.Drawing.Point(224, 36);
+            this.txtResultado.Multiline = true;
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(248, 131);
+            this.txtResultado.TabIndex = 6;
+            // 
+            // BSalir
+            // 
+            this.BSalir.ImageAlign = System.Drawing.ContentAlignment.BottomRight;
+            this.BSalir.Location = new System.Drawing.Point(408, 12);
+            this.BSalir.Name = "BSalir";
+            this.BSalir.Size = new System.Drawing.Size(64, 20);
+            this.BSalir.TabIndex = 7;
+            this.BSalir.Text = "&Salir";
+            this.BSalir.UseVisualStyleBackColor = true;
+            this.BSalir.Click += new System.EventHandler(this.BSalir_Click_1);
+            // 
+            // Formulario1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 261);
+            this.Controls.Add(this.BSalir);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.LNombre);
+            this.Controls.Add(this.LApellido);
+            this.Controls.Add(this.BEliminar);
+            this.Controls.Add(this.BGuardar);
+            this.KeyPreview = true;
+            this.Name = "Formulario1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Mi Primer Forms";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Formulario1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private Button BGuardar;
+        private Button BEliminar;
+        private Label LApellido;
+        private Label LNombre;
+        private TextBox txtApellido;
+        private TextBox txtNombre;
+        private TextBox txtResultado;
+        private Button BSalir;
+    }
+}
+
---
+++ Formulario1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practico1
+{
+    public partial class Formulario1 : Form
+    {
+        public Formulario1()
+        {
+            InitializeComponent();
+        }    
+
+        private void BGuardar_Click_1(object sender, EventArgs e)
+        {
+            txtResultado.Text = $"{txtApellido.Text} {txtNombre.Text}";
+        }
+
+        private void BEliminar_Click_1(object sender, EventArgs e)
+        {
+            txtResultado.Clear();
+        }
+
+        private void BSalir_Click_1(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Formulario1_KeyDown(object sender, KeyEventArgs e)
+        {
+            // Detecta si se presion� Ctrl + S
+            if (e.Control && e.KeyCode == Keys.S)
+            {
+                // Activa el bot�n que desees
+                BSalir.PerformClick();
+            }
+        }
+    }
+}
